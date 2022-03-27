f0613b69fb656638421ba2cb8789614e42f7aa10
Kalin Trachev <kalin.trachev@gmail.com>
2022-03-27 15:49:14 +0300

    Add and modify

---
+++ MAIN FILES/shared.js
@@ -0,0 +1,10 @@
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal')
+let selectPlanButtons = document.querySelectorAll('.plan button')
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function () {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block'
+    })
+}
