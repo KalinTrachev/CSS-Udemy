9c5c554f84f6b00d9fdcf89c7e2194bd8f7fdbc5
Kalin Trachev <kalin.trachev@gmail.com>
2022-03-27 22:58:07 +0300

    Add and modify

--- MAIN FILES/shared.js
+++ MAIN FILES/shared.js
@@ -1,10 +1,25 @@
 let backdrop = document.querySelector('.backdrop');
-let modal = document.querySelector('.modal')
-let selectPlanButtons = document.querySelectorAll('.plan button')
+let modal = document.querySelector('.modal');
+let selectPlanButtons = document.querySelectorAll('.plan button');
+let negativeButton = document.querySelector('.modal__action--negative');
+
+console.log(negativeButton);
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
         modal.style.display = 'block';
         backdrop.style.display = 'block'
     })
+}
+
+negativeButton.addEventListener('click', function () {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none'
+});
+
+backdrop.addEventListener('click', closeModal);
+
+function closeModal(){
+    backdrop.style.display = 'none';
+    modal.style.display = 'none
 }
