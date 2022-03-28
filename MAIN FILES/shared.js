8bc96a60fe89fd45b659200c8aeb8a69c363dd66
Kalin Trachev <kalin.trachev@gmail.com>
2022-03-28 15:53:58 +0300

    Add and modify

--- MAIN FILES/shared.js
+++ MAIN FILES/shared.js
@@ -2,24 +2,34 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let selectPlanButtons = document.querySelectorAll('.plan button');
 let negativeButton = document.querySelector('.modal__action--negative');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
-console.log(negativeButton);
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block'
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block'
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     })
 }
 
-negativeButton.addEventListener('click', function () {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none'
+negativeButton.addEventListener('click', closeModal);
+
+backdrop.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+    closeModal();
 });
 
-backdrop.addEventListener('click', closeModal);
-
 function closeModal(){
-    backdrop.style.display = 'none';
-    modal.style.display = 'none
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
+})
